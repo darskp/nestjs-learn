3fc29d0570728d95963c00fb42af8e820f85c0a0
darskp <darshanpatgar100@gmail.com>
2026-02-23 17:47:49 +0530

    Modules, Controllers and services

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { HelloService } from './hello.service';
 
 @Controller('hello')
+// The HelloController handles incoming requests to the /hello route and its sub-routes
+
 export class HelloController {
+    //constructor injection of HelloService to use its methods in the controller
     constructor(private readonly helloService: HelloService) {}
-    
-    @Get()
+
+    // http://localhost:3000/hello/first-route - returns a simple greeting
+    //example: /hello/first-route - returns "Hello from HelloService!"
+    @Get('first-route')
     getHello():String{
         return this.helloService.getHello();
+    }
+    
+    // /hello/user/:name - returns a personalized greeting using a route parameter
+    //example: /hello/user/John - returns "Hello, John! Welcome to NestJS!"
+    @Get('/user/:name')
+    getHelloWithName(@Param('name') name: string): string {
+        return this.helloService.getHelloWithName(name);
+    }
+    
+    // /hello/query?name=YourName - returns a personalized greeting using a query parameter
+    //example: /hello/query?name=John - returns "Hello, John! Welcome to NestJS!" or
+    //example: /hello/query - returns "Hello, Guest! Welcome to NestJS!" if no name is provided
+    //search or filter functionality on the FE
+    @Get('query')
+    getHelloWithQuery(@Query('name') name:string): string {
+        return this.helloService.getHelloWithName(name || 'Guest');
     }
 }
--- src/hello/hello.service.ts
+++ src/hello/hello.service.ts
@@ -3,7 +3,13 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 
 export class HelloService {
+    // This method returns a simple greeting message
     getHello(): string {
         return 'Hello from HelloService!';
+    }
+
+    // This method takes a name as a parameter and returns a personalized greeting message
+    getHelloWithName(name: string): string {
+        return `Hello, ${name}! Welcome to NestJS!`;
     }
 }
