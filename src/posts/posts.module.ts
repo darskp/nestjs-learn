25567cb67e9593c9d96bb2ea6697d49dcc8fed4b
darskp <darshanpatgar100@gmail.com>
2026-02-26 18:07:39 +0530

    Database connection with PostgreSQL & TypeORM

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from './entities/post.entity';
+import { PostsController } from './posts.controller';
+import { PostsService } from './posts.service';
+import { PostExistsPipe } from './customPipes/postExists.pipe';
 
-@Module({})
-export class PostsModule {}
+@Module({
+    imports: [
+        //available for dependency injection in the service and controller
+        TypeOrmModule.forFeature([Post])
+    ],
+    controllers: [PostsController],
+    providers: [PostsService, PostExistsPipe],
+    exports: [PostsService, PostExistsPipe],
+})
+export class PostsModule {
+
+}
